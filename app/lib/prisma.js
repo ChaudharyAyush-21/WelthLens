08e357107e6eec069db1958b51c64c4d068cc548
ChaudharyAyush-21 <ayushk2194@gmail.com>
2025-09-03 22:32:58 +0530

    adding final debt

--- app/lib/prisma.js
+++ app/lib/prisma.js
@@ -2,10 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis;
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query", "error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export { prisma };
+
+
--- components/header.jsx
+++ components/header.jsx
@@ -33,7 +33,7 @@ const Header = async () => {
             </Link>
 
              <Link
-              href="/debtpredictor"
+              href="/debt-tracker"
               className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
             >
               <Button variant="outline">
